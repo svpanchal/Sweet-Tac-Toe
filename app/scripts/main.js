a614e46f306cd097bcd3ada750b2d00b5eb66d09
Sorum Panchal <svpanchal@gmail.com>
2015-08-25 14:28:36 -0400

    Fixed grunt and syntax errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,10 @@
-'use strict';
-
 // jshint devel:true
-console.log('Tic Tac Toe');
+console.log('Tic Tac Toe!');
 
+//
 window.onload = start;
-var boxes = document.getElementsByTagName("td");
-var turnText = document.querySelector(".playerTurn");
+var boxes = document.getElementsByTagName('td');
+var turnText = document.querySelector('.playerTurn');
 var counter = 1;
 var winCounter = 0;
 var OMoves = [];
@@ -22,39 +21,64 @@ var winningCombinations = [
   [2, 4, 6]
 ];
 
-function start() {
-  playXandOListener();
-  addResetListener();
+//If this button is clicked, clear all boxes and reset the game.
+function resetBoard() {
+  'use strict';
+  for (var i = boxes.length - 1; i >= 0; i--) {
+    boxes[i].innerHTML = '';
+    boxes[i].setAttribute('class', 'clear');
+  }
+  OMoves = [];
+  XMoves = [];
+  winCounter = 0;
+  counter = 1;
+  turnText.innerHTML = 'Kick off the game, Player X!';
 }
 
-function playXandOListener() {
-  for (var i = boxes.length - 1; i >= 0; i--) {
-    boxes[i].addEventListener("click", playXorO);
+function checkForWin(movesArray, name) {
+  'use strict';
+  // loop over the first array of winning combinations
+  for (i = 0; i < winningCombinations.length; i++) {
+    // reset the winCounter each time
+    winCounter = 0;
+    // loop over each individual array
+    for (var j = 0; j < winningCombinations[i].length; j++) {
+      // If the number in winning combo array is === a number in moves array, add to winCounter
+      if (movesArray.indexOf(winningCombinations[i][j]) !== -1) {
+        winCounter++;
+      }
+      // If winCounter === 3, then all 3 moves comprise a winning combination and game is over and a winner is declared.
+      if (winCounter === 3) {
+      alert('Congrats Player ' + name + ', you won!');
+        resetBoard();
+      }
+    }
   }
 }
 
 function playXorO(event) {
+  'use strict';
   if (event.target.innerHTML.length === 0) {
     if (counter % 2 === 0) {
-      OMoves.push(parseInt(event.target.getAttribute("data-num")));
-      event.target.innerHTML = "O";
-      event.target.setAttribute("class", "O");
-      turnText.innerHTML = "It's your move, Player X";
+      OMoves.push(parseInt(event.target.getAttribute('data-num')));
+      event.target.innerHTML = 'O';
+      event.target.setAttribute('class', 'O');
+      turnText.innerHTML = 'Your turn, Player X';
       counter++;
-      checkForWin(OMoves, "O");
+      checkForWin(OMoves, 'O');
     }
     else {
-      XMoves.push(parseInt(event.target.getAttribute("data-num")));
-      event.target.innerHTML = "X";
-      event.target.setAttribute("class", "X");
-      turnText.innerHTML = "Your move, Player O";
+      XMoves.push(parseInt(event.target.getAttribute('data-num')));
+      event.target.innerHTML = 'X';
+      event.target.setAttribute('class', 'X');
+      turnText.innerHTML = 'Your move, Player O';
       counter++;
-      checkForWin(XMoves, "X");
+      checkForWin(XMoves, 'X');
     }
     // if the counter is greater than or equal to 10, declare the game a draw.
     if (counter >= 10) {
-      turnText.innerHTML = "Game Over!";
-      var conf = confirm("It's a draw, want a rematch?");
+      turnText.innerHTML = 'Game Over!';
+      var conf = confirm('It is a draw, want a rematch?');
       if (conf) {
         resetBoard();
       }
@@ -62,40 +86,23 @@ function playXorO(event) {
   }
 }
 
-//Button to reset gameboard and start new game
-function addResetListener() {
-  var resetButton = document.getElementById("reset");
-  resetButton.addEventListener("click", resetBoard);
-}
-
-function checkForWin(movesArray, name) {
-  // loop over the first array of winning combinations
-  for (i = 0; i < winningCombinations.length; i++) {
-    // reset the winCounter each time
-    winCounter = 0;
-    // loop over each individual array
-    for (var j = 0; j < winningCombinations[i].length; j++) {
-      // if the number in winning combo array is === a number in moves array, add to winCounter
-      if (movesArray.indexOf(winningCombinations[i][j]) !== -1) {
-        winCounter++;
-      }
-      // if winCounter === 3 that means all 3 moves are winning combos and game is over!
-      if (winCounter === 3) {
-        alert("Congrats " + name + ", you've won!");
-        resetBoard();
-      }
-    }
-  }
-}
-
-function resetBoard() {
+//Makes each cell clickable
+function playXandOListener() {
+  'use strict';
   for (var i = boxes.length - 1; i >= 0; i--) {
-    boxes[i].innerHTML = "";
-    boxes[i].setAttribute("class", "clear");
+    boxes[i].addEventListener('click', playXorO);
   }
-  OMoves = [];
-  XMoves = [];
-  winCounter = 0;
-  counter = 1;
-  turnText.innerHTML = "It's player X's turn";
+}
+
+//Makes 'Reset game' button clickable
+function addResetListener() {
+  'use strict';
+  var resetButton = document.getElementById('reset');
+  resetButton.addEventListener('click', resetBoard);
+}
+
+function start() {
+  'use strict';
+  playXandOListener();
+  addResetListener();
 }
